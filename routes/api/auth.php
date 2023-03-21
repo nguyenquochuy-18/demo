ba822fbed630132ceefe9bf5e1aa15dc0ff2c994
nguyenquochuy2k1 <dh51905009@student.stu.edu.vn>
2023-03-21 23:39:50 +0700

    update

--- routes/api/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:user');
--- website/docs/3-api-documentation/1-redoc.md
+++
@@ -1,9 +0,0 @@
----
-title: Redoc
-hide_title: true
-hide_table_of_contents: true
----
-
-import ApiDocMdx from '@theme/ApiDocMdx';
-
-<ApiDocMdx id="redoc-nested" />
